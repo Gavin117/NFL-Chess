71da9e5db61f2dff149d56398d6e45b65107087d
Gavin117 <100494117+Gavin117@users.noreply.github.com>
2022-03-05 21:50:22 +0000

    Add files via upload

---
+++ nfl_chess/nfl_chess.py
@@ -0,0 +1,86 @@
+import csv
+import time
+
+def get_data(file):
+    csvfile = open(file,newline='')
+    reader = csv.reader(csvfile)
+    header = next(reader)
+    data = [row for row in reader]
+    return data
+
+def get_start():
+    data = get_data('wl.csv')
+    team1 = [row[4] for row in data]
+    team2 = [row[6] for row in data]
+    teams = team1+team2
+    teams= set(teams)
+    start = []
+    for team in teams:
+        start.append([team,1000])
+    return start
+    
+
+def elo(playera,playerb):    
+    awins = 1/(1 + 10**((playerb-playera)/400))
+    print(awins)
+
+
+def get_rating(name):
+    for team in ratings:
+        if name in team:
+            return team[1]
+
+def update_winner(win,lose):
+    winner = get_rating(win)
+    loser = get_rating(lose)
+    wins = 1/(1 + 10**((loser-winner)/400))
+    update = (winner+32*(1-wins))
+    return update
+
+def update_loser(lose,win):
+    loser = get_rating(lose)
+    winner = get_rating(win)
+    wins = 1/(1 + 10**((winner-loser)/400))
+    update = (loser-32*(1-wins))
+    return update
+    
+    
+    
+def process_schedule(file):
+    data = get_data(file)
+    winners = [row[4] for row in data]
+    losers = [row[6] for row in data]
+    x = list(zip(winners,losers))
+    return x
+
+
+
+ratings = get_start()
+
+games_list = process_schedule('wl.csv')
+
+for game in games_list:
+    #print(game)
+
+    for team in ratings:
+        if game[0] in team:
+            winner = get_rating(game[0])
+            loser = get_rating(game[1])
+            team[1] = update_winner(winner, loser)
+            #print(team[1])
+        if game[1] in team:
+            winnr = get_rating(game[0])
+            losr = get_rating(game[1])
+            team[1] = update_loser(losr, winnr)
+            #print(team[1])
+            
+ratings.sort(key= lambda x: x[1])
+
+for result in reversed(ratings):
+    print(result)
+    
+
+
+
+
+
